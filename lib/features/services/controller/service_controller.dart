7b2396034a115712518bf1a508d5d6e51ae6b785
risanpallithazha <risanpallithazha@gmail.com>
2024-12-02 11:26:15 +0530

    Service Api Functions Completed

--- lib/features/services/controller/service_controller.dart
+++ lib/features/services/controller/service_controller.dart
@@ -12,9 +12,39 @@ class AddServiceController extends _$AddServiceController {
     final response = await ServicesApiServices().getService();
    return response as ServiceModel;
   }
+  //get services
   Future<void>getServices()async{
     try{
       await ServicesApiServices().getService();
+    }catch(e){
+      throw Exception(e);
+    }
+  }
+  
+  //add service
+  Future<ServiceModel>addService({required name,required category,required description,required price,required imageUrls})async{
+    try{
+      final response = await ServicesApiServices().addService(name: name, category: category, description: description, price: price, imageUrls: imageUrls);
+      return response;
+    }catch(e){
+      throw Exception(e);
+    }
+  }
+
+  //update service
+  Future<ServiceModel>updateService({required id,required name,required category,required description,required price,required imageUrls})async{
+    try{
+      final response = await ServicesApiServices().updateService(id: id, name: name, category: category, description: description, price: price, imageUrls: imageUrls);
+      return response;
+    }catch(e){
+      throw Exception(e);
+    }
+  }
+
+  //delete service
+  Future<void>deleteService({required id})async{
+    try{
+      await ServicesApiServices().deleteService(id: id);
     }catch(e){
       throw Exception(e);
     }
--- lib/features/services/service/services_api_services.dart
+++ lib/features/services/service/services_api_services.dart
@@ -1,13 +1,15 @@
 import 'package:dio/dio.dart';
+import 'package:handcar_ventor/core/service/base_url.dart';
 import 'package:handcar_ventor/features/services/controller/model/service_model.dart';
 
 class ServicesApiServices {
   static final Dio dio = Dio(
     BaseOptions(
-      baseUrl: 'https://jsonplaceholder.typicode.com/',
+      baseUrl: baseUrl,
     ),
   );
 
+ // get all services
   Future<List<ServiceModel>>getService()async{
    try{
      final response = await dio.get('/posts');
@@ -16,4 +18,44 @@ class ServicesApiServices {
      throw Exception(e);
    }
   }
+  Future<ServiceModel>addService({required name,required category,required description,required price,required imageUrls})async{
+   try{
+     final response = await dio.post('/posts',data: FormData.fromMap({
+       'name':name,
+       'category':category,
+       'description':description,
+       'price':price,
+       'imageUrls':imageUrls
+     }));
+     return ServiceModel.fromJson(response.data);
+   }catch(e){
+     throw Exception(e);
+   }
+
+  }
+  Future<ServiceModel>updateService({required id,required name,required category,required description,required price,required imageUrls})async{
+   try{
+     final response = await dio.put('/posts/$id',data: FormData.fromMap({
+       'name':name,
+       'category':category,
+       'description':description,
+       'price':price,
+       'imageUrls':imageUrls
+     }));
+     return ServiceModel.fromJson(response.data);
+   }catch(e){
+     throw Exception(e);
+   }
+
+  }
+
+  // delete service
+  Future<void>deleteService({required id})async{
+   try{
+     await dio.delete('/posts/$id');
+   }catch(e){
+     throw Exception(e);
+   }
+  }
+
 }
