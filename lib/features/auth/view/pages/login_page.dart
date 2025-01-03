5162f70ae4ffd72c695c602e4b6870c4c4cc3065
risanpallithazha <risanpallithazha@gmail.com>
2025-01-03 14:22:26 +0530

    Login page modified

--- lib/features/auth/view/pages/login_page.dart
+++ lib/features/auth/view/pages/login_page.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/svg.dart';
